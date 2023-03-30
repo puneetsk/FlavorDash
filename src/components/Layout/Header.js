9736b72cb22d8a8e404c888dc85c5d3dbf05b471
Puneet Kolathaya <pkolatha@hawk.iit.edu>
2023-03-30 12:07:13 -0500

    Initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <React.Fragment>
+            <header>
+                <h1>FlavorDash</h1>
+                <button>
+                    Cart
+                </button>
+            </header>
+
+            <div className=''>
+
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Header;
