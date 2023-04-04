23bb2ea870919a0c453549e4acf0eae4526243ca
Puneet Kolathaya <pkolatha@hawk.iit.edu>
2023-04-04 16:34:36 -0500

    Completing the animation when the item is added to the cart.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,38 @@
-import {useContext} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartButtonStyles from './HeaderCartButton.module.css';
 import cartContext from '../../store/cart-context';
 
 
+
 const HeaderCartButton = (props) => {
     const headerContext = useContext(cartContext);
-    const numberofCartItems = headerContext.items.reduce((currentNum, item) => {
+    const { items } = headerContext;
+    const numberofCartItems = items.reduce((currentNum, item) => {
         return currentNum + item.amount
     }, 0);
-    return <button className={CartButtonStyles.button} onClick={props.onClick}>
+
+
+    const [isHighlighted, setisHighlighted] = useState(false);
+    const buttonBump = `${CartButtonStyles.button} ${isHighlighted ? CartButtonStyles.bump : ''}`;
+
+
+    useEffect(()=> {
+
+        if(items.length === 0){
+            return;
+        }
+        setisHighlighted(true);
+        const timer = setTimeout(() => {
+            setisHighlighted(false);
+        }, 300)
+      
+        return () => {
+            clearTimeout(timer);
+        };    
+    }, [items]);
+
+    return <button className={buttonBump} onClick={props.onClick}>
         <span className={CartButtonStyles.icon}><CartIcon/></span>
         <span>Your Cart</span>        
         <span className={CartButtonStyles.badge}>{numberofCartItems}</span>
